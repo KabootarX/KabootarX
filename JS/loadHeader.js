1bba993ca5dcbbd3e917e05a7a8378f60e84181a
Nikki Kumari <137755944+nikki30j@users.noreply.github.com>
2024-08-18 12:19:10 +0530

    Update loadHeader.js

--- JS/loadHeader.js
+++ JS/loadHeader.js
@@ -1,5 +1,5 @@
 function loadHeader() {
-    fetch('../header.html')
+    fetch('https://github.com/KabootarX/KabootarX/blob/main/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('headerContainer').innerHTML = data;
